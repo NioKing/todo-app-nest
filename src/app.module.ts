e3a00cbefa8f0169e5c5e52e1ad71989a96eddca
Nio <nio@example.com>
2022-08-14 17:37:13 +0300

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { TodoModule } from './todo/todo.module';
     password: '12c813c9f803a6b7d239de8b07806e6f4c4b0ef3303645493392fa34855bb908',
     database: 'da3q1f6ulqt09n',
     entities: ["dist/**/entities/*.entity{.ts,.js}"],
+    url: "postgres://zoiktuefaskvho:12c813c9f803a6b7d239de8b07806e6f4c4b0ef3303645493392fa34855bb908@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/da3q1f6ulqt09n",
     synchronize: true,
     autoLoadEntities: true
   }), CategoryModule, TodoModule
