4867901f9a133e6964bee2cd655e43d3e1d5a313
Nio <nio@example.com>
2022-08-15 13:31:58 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,13 +14,13 @@ import { TodoModule } from './todo/todo.module';
     introspection: true
   }),TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'ec2-34-249-161-200.eu-west-1.compute.amazonaws.com',
+    host: 'ec2-54-194-211-183.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    username: 'cipvvacrmzaeha',
-    password: '2326012bc44fb0ad843d08b864903f171402bdf2489cd130e5e233201e87ff56',
-    database: 'd5mb8nru1fpo7r',
+    username: 'hheabjwjwslctb',
+    password: 'ec3c8e624f6e145471ddbebe477a467002558390c008ef51c5a78648a9ac2563',
+    database: 'd64rqr8nqg1v0l',
     entities: ["dist/**/entities/*.entity{.ts,.js}"],
-    url: "postgres://cipvvacrmzaeha:2326012bc44fb0ad843d08b864903f171402bdf2489cd130e5e233201e87ff56@ec2-34-249-161-200.eu-west-1.compute.amazonaws.com:5432/d5mb8nru1fpo7r",
+    url: "postgres://hheabjwjwslctb:ec3c8e624f6e145471ddbebe477a467002558390c008ef51c5a78648a9ac2563@ec2-54-194-211-183.eu-west-1.compute.amazonaws.com:5432/d64rqr8nqg1v0l",
     synchronize: true,
     autoLoadEntities: true,
     ssl: {
