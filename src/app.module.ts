1fd3a209614b27dff49d1dc73ba48a9a1acc08e8
Nio <nio@example.com>
2022-08-15 12:23:00 +0300

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { TodoModule } from './todo/todo.module';
     introspection: true
   }),TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'ec2-52-208-164-5.eu-west-1.compute.amazonaws.com',
+    host: 'ec2-54-194-211-183.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    username: 'wwfueobicaxqdj',
-    password: '64af5aaef68d1fc7969c29559289f8ef4ad5dfc4713c56e6c67b72a0b5831e0b',
-    database: 'd57eiil7mtd791',
+    username: 'lvposlfekpnisi',
+    password: '888f9ceb49a730f7367e1de392cb592be6ca5e9004dc83ec9cd3699d5b2d20e5',
+    database: 'danu6f82ovjvom',
     entities: ["dist/**/entities/*.entity{.ts,.js}"],
     // url: "postgres://wwfueobicaxqdj:64af5aaef68d1fc7969c29559289f8ef4ad5dfc4713c56e6c67b72a0b5831e0b@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/d57eiil7mtd791",
     synchronize: true,
