4a64e462e0d7a71590aca1781474cb4531bc3526
Nio <nio@example.com>
2022-08-15 15:19:27 +0300

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { TodoModule } from './todo/todo.module';
     // password: 'ec3c8e624f6e145471ddbebe477a467002558390c008ef51c5a78648a9ac2563',
     // database: 'd64rqr8nqg1v0l',
     entities: ["dist/**/entities/*.entity{.ts,.js}"],
-    url: "postgres://hheabjwjwslctb:ec3c8e624f6e145471ddbebe477a467002558390c008ef51c5a78648a9ac2563@ec2-54-194-211-183.eu-west-1.compute.amazonaws.com:5432/d64rqr8nqg1v0l",
+    url: "postgres://xpkenkxatotpat:c913783728e102cc1f714137a6b19ae83ac3c8923a86e8f0201feb88a8b33803@ec2-34-252-216-149.eu-west-1.compute.amazonaws.com:5432/d2chf9ir6did57",
     synchronize: true,
     autoLoadEntities: true,
     ssl: {
